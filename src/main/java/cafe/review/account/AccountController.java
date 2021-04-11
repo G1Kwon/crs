425099aa91acb9f9eb1ef6c6d4aeae3f917f0db4
g1.kwon <spring_shot@naver.com>
2021-04-11 13:00:18 +0900

    add sign-up Form validation

--- src/main/java/cafe/review/account/AccountController.java
+++ src/main/java/cafe/review/account/AccountController.java
@@ -1,11 +1,28 @@
 package cafe.review.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    //바인딩 설정
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
@@ -14,4 +31,12 @@ public class AccountController {
         return "account/sign-up";
     }
 
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid @ModelAttribute SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()){
+            return "account/sign-up";
+        }
+        // TODO 화원 가입 처리
+        return "redirect:/";
+    }
 }
