4f41f045fe8060d5b9fff544a202da816f4faee3
g1.kwon <spring_shot@naver.com>
2021-04-18 23:01:54 +0900

    관심 주제 조회

--- src/main/java/cafe/review/account/AccountService.java
+++ src/main/java/cafe/review/account/AccountService.java
@@ -21,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -130,5 +131,10 @@ public class AccountService implements UserDetailsService {
     public void addTag(Account account, Tag tag) {
         Optional<Account> byId = accountRepository.findById(account.getId());
         byId.ifPresent(a -> a.getTags().add(tag));
+    }
+
+    public Set<Tag> getTags(Account account) {
+        Optional<Account> byId = accountRepository.findById(account.getId());
+        return byId.orElseThrow().getTags();
     }
 }
