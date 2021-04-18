1e5bb03b9536948132dd661a730026896ca2555f
g1.kwon <spring_shot@naver.com>
2021-04-18 22:37:10 +0900

    관심 주제 도메인

--- src/main/java/cafe/review/domain/Account.java
+++ src/main/java/cafe/review/domain/Account.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -54,6 +55,9 @@ public class Account {
     private boolean cafeUpdatedByEmail;
 
     private boolean cafeUpdatedByWeb = true;
+
+    @ManyToMany
+    private Set<Tag> tags;
 
     public void generateEmailCheckToken() {
         this.emailCheckToken = UUID.randomUUID().toString();
