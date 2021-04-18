9afe502c78c08cc87f44010e4ed8225c629c4569
g1.kwon <spring_shot@naver.com>
2021-04-18 23:37:36 +0900

    지역 도메인

---
+++ src/main/java/cafe/review/zone/ZoneRepository.java
@@ -0,0 +1,7 @@
+package cafe.review.zone;
+
+import cafe.review.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+}
